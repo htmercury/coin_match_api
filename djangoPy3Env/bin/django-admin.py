db260f5dbc0918591f039822e3201266ff12a2b8
Ka Wong <kawong2020@u.northwestern.edu>
2018-07-23 16:43:28 -0500

    remove venv

--- djangoPy3Env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/zacharyverghese/Downloads/coin_app/coin_match_api/djangoPy3Env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
