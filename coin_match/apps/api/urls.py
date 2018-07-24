6a49ed6960e2a6aea686baa91ffba9ded6511537
Ka Wong <kawong2020@u.northwestern.edu>
2018-07-24 15:18:22 -0500

    Added unit tests

--- coin_match/apps/api/urls.py
+++ coin_match/apps/api/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     url(r'^cryptocurrency$', views.CryptoCreate.as_view()),
     url(r'^cryptocurrency/(?P<pk>\d+)$', views.CryptoView.as_view()),
-    url(r'^exchange$', views.ExchangeCreate.as_view()),
+    url(r'^exchange$', views.ExchangeCreate.as_view(), name='create'),
     url(r'^exchange/(?P<pk>\d+)$', views.ExchangeView.as_view()),
     url(r'^transaction$', views.TransactionCreate.as_view()),
-    url(r'^transactiTn/(?P<pk>\d+)$', views.TransactionView.as_view())
+    url(r'^transaction/(?P<pk>\d+)$', views.TransactionView.as_view())
 ]
